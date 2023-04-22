62c89895df912db28d1065ef59d698cf2510251f
Grace Crowell <gmcrow20@gmail.com>
2023-04-22 13:28:48 -0400

    Initial Commit

---
+++ Lesson13/Lesson13App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson13App.swift
+//  Lesson13
+//
+//  Created by Grace Crowell on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Lesson13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
